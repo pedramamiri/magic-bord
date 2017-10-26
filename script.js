7edb6345cf508812aa00ca525c614bfdc4933488
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2017-10-26 15:58:14 +0200

    hi2

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+var noOfSquare = 8;
 var contentDiv = document.getElementById("content");
 var board=document.createElement("div");
 board.className="board";
 contentDiv.appendChild(board);
-for( var y=0; y<8; y++){
-    addRowToBoard()
+for( var y=0; y<noOfSquare; y++){
+    var row = addRowToBoard()
+    for(var x=0; x<noOfSquare; x++) {
+    var square = document.createElement("div");
+    square.className="square";
+    row.appendChild(square);
+    if((x+y) % 2){
+        square.style.backgroundColor = "black";
+        }else {
+            square.style.backgroundColor = "white";
+        
+        }
+    }
 }
 
 contentDiv.appendChild(board);
@@ -15,9 +27,14 @@ function addRowToBoard(){
         if(y%2){
         row.style.backgroundColor = "black";
         }else {
-            row.style.backgroundColor = "red";
+            row.style.backgroundColor = "white";
         
         }
-        
-        }
+        board.appendChild(row);
+        return row ;
 
+    
+        
+    }
+
+    
